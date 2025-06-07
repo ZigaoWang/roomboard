e49ee647a3f153156a2bed98e9147d0a83aa9218
Zigao Wang <zigao.wang24@gmail.com>
2025-06-07 10:36:35 +0800

    refractor: new project

---
+++ RoomBoard/RoomBoard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoomBoard
+//
+//  Created by Zigao Wang on 6/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoomBoard/RoomBoard/RoomBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomBoardApp.swift
+//  RoomBoard
+//
+//  Created by Zigao Wang on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RoomBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
